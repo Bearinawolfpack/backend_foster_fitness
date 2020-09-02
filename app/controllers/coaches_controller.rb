2c4c5179f9ab1381d45a9eb07608cc8d4d24d653
bearinawolfpack <bearinawolfpack@gmail.com>
2020-09-02 09:07:54 -0400

    Adjusted Coaches controller with CRUD actions

--- app/controllers/coaches_controller.rb
+++ app/controllers/coaches_controller.rb
@@ -9,4 +9,25 @@ class CoachesController < ApplicationController
         @coaches = Coach.all
         render json: @coaches
     end
+    
+    def create
+        @coach = Coach.create(coach_params)
+    end
+
+    def update
+        @coach = Coach.find(params[:id])
+        @coach.update(coach_params)
+        render json: @coach
+    end
+
+    def destroy
+        @coach = Coach.find(params[:id])
+        @coach.destroy
+    end
+
+    private
+
+    def coach_params
+        params.require(:coach).permit(:name, :bio, :picture, :admin_id)
+    end
 end
