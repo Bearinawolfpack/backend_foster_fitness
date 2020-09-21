6b4bef17e13d1f092fe7d2c0f39b7a56bf0cdcc3
bearinawolfpack <bearinawolfpack@gmail.com>
2020-09-21 18:42:25 -0400

    Updated README

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -8,4 +8,26 @@ class EventsController < ApplicationController
         @events = Event.all
         render json: @events
     end
+
+    def create
+        @event = Event.create(event_params)
+    end
+
+    def update
+        @event = Event.find(params[:id])
+        @event.update(event_params)
+        render json: @event
+    end
+
+    def destroy
+        @event = Event.find(params[:id])
+        @event.destroy
+    end
+
+    private
+
+    def event_params
+        params.require(:event).permit(:name, :description, :picture, :url, :admin_id)
+    end
 end
+
